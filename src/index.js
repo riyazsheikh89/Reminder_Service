76c0c381fafb9c91cc83d8500804803162936e89
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-01-17 17:11:06 +0530

    basic setup done

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const {PORT} = require('./config/serverConfig');
+
+const setupAndStartServer = () => {
+
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server Started on PORT: ${PORT}`);
+    })
+}
+
+setupAndStartServer();
