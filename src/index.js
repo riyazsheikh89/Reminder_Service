72ddb322ce285844676c6829e6761b98fde67ce4
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-09-25 15:48:36 +0530

    RabbitMQ setup code added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cron = require('node-cron')
 const {PORT} = require('./config/serverConfig');
 const Jobs = require('./utils/job');
 const TicketController = require('./controllers/ticket-controller');
+const { createChannel } = require("./utils/message_queue");
 
 const setupAndStartServer = () => {
 
@@ -11,11 +12,13 @@ const setupAndStartServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    // const channel = createChannel();
+
     app.post('/api/v1/tickets', TicketController.create);
 
     app.listen(PORT, () => {
         console.log(`Server Started on PORT: ${PORT}`);
-        Jobs();
+        // Jobs();
 
     })
 }
